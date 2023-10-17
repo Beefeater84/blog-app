84a4ad230dcb09ed8c61643b826a047217fe4b52
Beefeater <myasoedov.blog@gmail.com>
2023-10-17 21:32:53 +0300

    auth btn

--- src/features/authButton/authButton.tsx
+++ src/features/authButton/authButton.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 export default function AuthButton() {
+  const { data, status } = useSession();
+
+  console.log(data, status);
+
   return (
     <div>
-      <button type="button">Auth</button>
+      <button type="button" onClick={() => signIn("google")}>
+        Auth
+      </button>
     </div>
   );
 }
