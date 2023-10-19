097e84e003140ddf844dc3adc5f78efe98ee3fea
Beefeater <myasoedov.blog@gmail.com>
2023-10-19 14:27:35 +0300

    add prisma adapter to NextAuth

--- src/application/auth/providers.ts
+++ src/application/auth/providers.ts
@@ -1,6 +1,9 @@
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/application/db/connets";
 
 const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
