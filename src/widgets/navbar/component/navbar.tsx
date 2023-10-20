4b3e9741bf9fa5754de32df6314df45dce775c28
Beefeater <myasoedov.blog@gmail.com>
2023-10-20 11:27:53 +0300

    add navbar

--- src/widgets/navbar/component/navbar.tsx
+++ src/widgets/navbar/component/navbar.tsx
@@ -4,23 +4,35 @@ import AuthButtons from "@/widgets/navbar/component/authButtons";
 
 export default function Navbar() {
   return (
-    <div>
-      <div>HIKING BLOG</div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="#">Home</Link>
-          </li>
-          <li>
-            <Link href="#">Blog</Link>
-          </li>
-          <li>
-            <Link href="#">About</Link>
-          </li>
-        </ul>
-        <AuthButtons />
+    <div className="flex justify-between items-center h-[80px] container border-b border-stroke-color">
+      <div className="flex gap-2 items-center">
+        <div className="font-poppins font-semibold text-xl text-logo-color mr-3">
+          HIKING BLOG
+        </div>
+        <nav>
+          <ul className="flex gap-[28px] font-poppins font-semibold text-menu-color tracking-{0.2px}">
+            <li className="">
+              <Link href="#" className="hover:text-active-color">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:text-active-color">
+                Categories
+              </Link>
+            </li>
+            <li>
+              <Link href="#" className="hover:text-active-color">
+                Most popular posts
+              </Link>
+            </li>
+          </ul>
+        </nav>
         <ThemeToggle />
-      </nav>
+      </div>
+      <div>
+        <AuthButtons />
+      </div>
     </div>
   );
 }
