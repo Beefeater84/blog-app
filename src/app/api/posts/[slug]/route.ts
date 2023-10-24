b76e293b26b0a0031b290fe0af59bd649e6b6e86
Beefeater <myasoedov.blog@gmail.com>
2023-10-24 22:48:09 +0300

    add files

---
+++ src/app/api/posts/[slug]/route.ts
@@ -0,0 +1,43 @@
+import prisma from "@/application/db/connets";
+import { POST_PER_PAGE } from "@/entities/posts/const";
+
+// eslint-disable-next-line import/prefer-default-export
+export const GET = async (req: Request) => {
+  const { searchParams } = new URL(req.url || "");
+  const page = searchParams.get("page") || 1;
+  const cat = searchParams.get("cat") || undefined;
+
+  const where = cat ? { categorySlug: cat } : {};
+
+  const queryParams = {
+    take: POST_PER_PAGE,
+    skip: POST_PER_PAGE * (+page - 1),
+    include: {
+      user: true,
+    },
+    where,
+  };
+
+  try {
+    const [posts, count] = await prisma.$transaction([
+      prisma.post.findMany(queryParams),
+      prisma.post.count({
+        where,
+      }),
+    ]);
+
+    return Response.json(
+      { posts, count },
+      {
+        status: 200,
+      },
+    );
+  } catch (e) {
+    return Response.json(
+      { message: "Something went wrong!" },
+      {
+        status: 500,
+      },
+    );
+  }
+};
