5b5ed9b1997d7ec139e4268a2b5224f902ed0207
Beefeater <myasoedov.blog@gmail.com>
2023-10-26 20:22:16 +0300

    show comments

--- src/widgets/comments/comments.tsx
+++ src/widgets/comments/comments.tsx
@@ -1,90 +1,57 @@
 import LeaveCommentForm from "@/features/leaveComment/leave-comment";
 
-export default function Comments() {
+async function getComment(slug: string) {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_HOST}/api/comments?postSlug=${slug}`,
+    { next: { tags: ["comments"] } },
+  );
+  return res.json();
+}
+
+interface CommentProps {
+  slug: string;
+}
+
+export default async function Comments({ slug }: CommentProps) {
+  const comments = await getComment(slug);
+
   return (
     <div className="container">
-      <LeaveCommentForm />
+      <LeaveCommentForm postSlug={slug} />
       <h2> Comments </h2>
-      <div className="flex gap-[1rem] bg-white dark:bg-black-light-color rounded-xl p-[1rem] mb-[1rem]">
-        <div className="shrink-0">
-          <img
-            src="https://lh3.googleusercontent.com/a/ACg8ocKEwZYt-WsKLuAyJy9bgc-DvKWA9kel8fuTsslErRcPQ4A=s96-c"
-            alt="comment author"
-            className="rounded-full w-[40px] h-[40px]"
-          />
-        </div>
-        <div className="w-[100%]">
-          <div className="flex justify-between items-start pb-[0.5rem]">
-            <span className="text-sm text-blue-color dark:text-black-title-color font-medium">
-              Any Carrol
-            </span>
-            <span className="text-xs text-menu-color font-roboto">12:48PM</span>
-          </div>
-          <div className="text-xs font-roboto text-menu-color ">
-            Hey Cak, Could you free now? Can you look and read the brief first
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-[1rem] bg-white dark:bg-black-light-color rounded-xl p-[1rem] mb-[1rem]">
-        <div className="shrink-0">
-          <img
-            src="https://lh3.googleusercontent.com/a/ACg8ocKEwZYt-WsKLuAyJy9bgc-DvKWA9kel8fuTsslErRcPQ4A=s96-c"
-            alt="comment author"
-            className="rounded-full w-[40px] h-[40px]"
-          />
-        </div>
-        <div className="w-[100%]">
-          <div className="flex justify-between items-start pb-[0.5rem]">
-            <span className="text-sm text-blue-color dark:text-black-title-color font-medium">
-              Any Carrol
-            </span>
-            <span className="text-xs text-menu-color font-roboto">12:48PM</span>
-          </div>
-          <div className="text-xs font-roboto text-menu-color ">
-            Hey Cak, Could you free now? Can you look and read the brief first
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-[1rem] bg-white dark:bg-black-light-color rounded-xl p-[1rem] mb-[1rem]">
-        <div className="shrink-0">
-          <img
-            src="https://lh3.googleusercontent.com/a/ACg8ocKEwZYt-WsKLuAyJy9bgc-DvKWA9kel8fuTsslErRcPQ4A=s96-c"
-            alt="comment author"
-            className="rounded-full w-[40px] h-[40px]"
-          />
-        </div>
-        <div className="w-[100%]">
-          <div className="flex justify-between items-start pb-[0.5rem]">
-            <span className="text-sm text-blue-color dark:text-black-title-color font-medium">
-              Any Carrol
-            </span>
-            <span className="text-xs text-menu-color font-roboto">12:48PM</span>
-          </div>
-          <div className="text-xs font-roboto text-menu-color ">
-            Hey Cak, Could you free now? Can you look and read the brief first
-          </div>
-        </div>
-      </div>
-      <div className="flex gap-[1rem] bg-white dark:bg-black-light-color rounded-xl p-[1rem] mb-[1rem]">
-        <div className="shrink-0">
-          <img
-            src="https://lh3.googleusercontent.com/a/ACg8ocKEwZYt-WsKLuAyJy9bgc-DvKWA9kel8fuTsslErRcPQ4A=s96-c"
-            alt="comment author"
-            className="rounded-full w-[40px] h-[40px]"
-          />
-        </div>
-        <div className="w-[100%]">
-          <div className="flex justify-between items-start pb-[0.5rem]">
-            <span className="text-sm text-blue-color dark:text-black-title-color font-medium">
-              Any Carrol
-            </span>
-            <span className="text-xs text-menu-color font-roboto">12:48PM</span>
-          </div>
-          <div className="text-xs font-roboto text-menu-color ">
-            Hey Cak, Could you free now? Can you look and read the brief first
-          </div>
-        </div>
-      </div>
+      {comments &&
+        comments.map((comment) => {
+          const { id, description, createdAt, author } = comment;
+          const { name, image } = author;
+
+          return (
+            <div
+              key={id}
+              className="flex gap-[1rem] bg-white dark:bg-black-light-color rounded-xl p-[1rem] mb-[1rem]"
+            >
+              <div className="shrink-0">
+                <img
+                  src={image}
+                  alt="comment author"
+                  className="rounded-full w-[40px] h-[40px]"
+                />
+              </div>
+              <div className="w-[100%]">
+                <div className="flex justify-between items-start pb-[0.5rem]">
+                  <span className="text-sm text-blue-color dark:text-black-title-color font-medium">
+                    {name}
+                  </span>
+                  <span className="text-xs text-menu-color font-roboto">
+                    {createdAt}
+                  </span>
+                </div>
+                <div className="text-xs font-roboto text-menu-color ">
+                  {description}
+                </div>
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
