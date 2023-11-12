3d56a25daf3cdb008f11a7f93390bc4d3e3fcf95
Beefeater <myasoedov.blog@gmail.com>
2023-11-12 11:07:50 +0200

    try to fix Document error in prod

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -2,16 +2,20 @@
 
 import "react-quill/dist/quill.snow.css";
 import "./ReactQuill.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import toSlug from "@/shared/utilities/toSlug";
+import ReactQuill from "react-quill";
 
 export default function Page() {
-  // const [value, setValue] = useState("");
-  const value = "";
+  const [value, setValue] = useState("");
+  const [readyForBuild, setReadyForBuild] = useState(false);
   const [title, setTitle] = useState<string>("");
   const { status, data } = useSession();
 
+  useEffect(() => {
+    setReadyForBuild(true);
+  }, []);
   const createPostHandler = async () => {
     await fetch("/api/posts", {
       method: "POST",
@@ -28,7 +32,7 @@ export default function Page() {
     });
   };
 
-  if (status === "loading") {
+  if (status === "loading" || !readyForBuild) {
     return <div>Loading...</div>;
   }
 
@@ -51,12 +55,12 @@ export default function Page() {
       </div>
       <div className="container">
         <article>
-          {/* <ReactQuill */}
-          {/*  theme="snow" */}
-          {/*  value={value} */}
-          {/*  onChange={setValue} */}
-          {/*  placeholder="Start here" */}
-          {/* /> */}
+          <ReactQuill
+            theme="snow"
+            value={value}
+            onChange={setValue}
+            placeholder="Start here"
+          />
         </article>
         <button type="button" onClick={createPostHandler}>
           Create a post
